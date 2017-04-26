25c0d9540a95c2a4023e02b71bd8d9708dcd40ed
Chad Kirby <chad.kirby@gmail.com>
2017-04-25 18:21:11 -0700

    rejigger namespace logic so I can use it separately from xml-properties

--- src/index.js
+++ src/index.js
@@ -11,3 +11,4 @@ exports.GetMixin = require('./get-mixin');
 exports.makeNSTagSelector = require('./make-ns-tag-selector');
 
 exports.XmlPropertiesMixin = require('./xml-properties-mixin');
+exports.XmlNamespaceMixin = require('./xml-namespace-mixin');
--- src/make-ns-tag-selector.js
+++ src/make-ns-tag-selector.js
@@ -1,42 +1,43 @@
 let re = require('block-re');
 
-const needsNamespace = re('g')`
-(
-  // a thing that needs a namespace can begin the string; but the beginning of
-  // the string does not need a namespace if it begins with 1+ word chars
-  // followed by a colon
-  ^ (?! \w+ \\* :)
-  |
-  // or a thing that needs a namespace can be preceded by any of several chars
-
-  // a namespace-needing thing can be preceded by an open bracket e.g.,
-  // [foo="1"] => [w:foo="1"]
-
-  // a namespace-needing thing can follow an open paren, e.g.,
-  // :has(foo) => :has(w:foo)
-
-  // a namespace-needing thing can follow a comma, e.g.,
-  // foo,bar => w:foo,w:bar
-
-  // a namespace-needing thing can follow whitespace, e.g.,
-  // foo bar => w:foo w:bar
-
-  [ [ ( , \s ]
-) // close capture group of the stuff before the namespace-needing thing
-
-// capture the namespace-needing thing
-(\w+)
-// but the thing does not need a namespace if it is the namespace (followed by a
-// colon)
-(?! \\* : )
-`
-
-function makeNSTagSelector(namespace) {
+function makeNSTagSelector(namespace = '') {
   //
   // w is a template-tag function for escaping* and name-spacing ooxml selectors
   // e.g., w`p` => `w\\:p`
   // * cheerio requires that the colon following the namespace be escaped
   //
+  const needsNamespace = re('g')`
+  (
+    // a thing that needs a namespace can begin the string
+    ^
+    |
+    // or a thing that needs a namespace can be preceded by any of several chars
+
+    // a namespace-needing thing can be preceded by an open bracket e.g.,
+    // [foo="1"] => [w:foo="1"]
+
+    // a namespace-needing thing can follow an open paren, e.g.,
+    // :has(foo) => :has(w:foo)
+
+    // a namespace-needing thing can follow a comma, e.g.,
+    // foo,bar => w:foo,w:bar
+
+    // a namespace-needing thing can follow whitespace, e.g.,
+    // foo bar => w:foo w:bar
+
+    [ [ ( , \s ]
+  ) // close capture group of the stuff before the namespace-needing thing
+
+  // thing does not need a namespace if it is the namespace
+  (?! ${namespace} \\* :)
+
+  // capture the namespace-needing thing
+  (\w+)
+  // but the thing does not need a namespace if it is the namespace (followed by a
+  // colon)
+  (?! \\* : )
+  `
+
   return function(strings, ...keys) {
     let out = '';
     for (let index = 0; index < strings.length; index++) {
